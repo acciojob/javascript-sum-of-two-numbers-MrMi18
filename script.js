9fc59b72da4e5bf4423974421d72e2a9cf35a385
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-04-09 16:09:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
+let a = parseInt(input1);
+let b = parseInt(input2);
+if(!a || !b)alert("Invalid input. Please enter a valid number.");
+else alert(a+b);
 
 // Write your code here and print the output using alert function
